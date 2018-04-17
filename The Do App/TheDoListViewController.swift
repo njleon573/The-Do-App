4f943c44620017d3f85cdf28158fb21212dd77ad
Nick John <Nick@nicks-macbook-pro.local>
2018-04-17 11:34:15 -0700

    add new item functionality complete

--- The Do App/TheDoListViewController.swift
+++ The Do App/TheDoListViewController.swift
@@ -10,19 +10,37 @@ import UIKit
 
 class TheDoListViewController: UITableViewController {
     
-    let itemArray = ["Buy Weed","Buy Molly","Buy Mushrooms","Buy Drugs"]
+    
+    var itemArray = ["Buy Weed","Buy Molly","Buy Mushrooms","Buy Drugs"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    
-    @IBAction func addButtonPressed(_ sender: Any) {
+    //MARK - Add Button Functionality
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New The Do Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will happen when user clicks add button on Alert.
+       self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+      
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        return itemArray.count
     }
     
+    //MARK - TableViewMethods
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TheDoItemCell")!
         
